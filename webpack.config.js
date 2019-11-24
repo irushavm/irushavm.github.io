e34b5c543bee8e8e598e3f11ea5298d5c88a2b6c
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2019-11-24 12:28:26 -0500

    Update npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, 'dist/');
@@ -16,16 +16,17 @@ var config = {
   },
   // devtool: 'inline-source-map',
   module : {
-    loaders : [
+    rules : [
       {
         test :/\.(scss|sass)?/i,
         include : [
           SCSS_DIR
         ],
-        use: ExtractTextPlugin.extract({ 
-          fallback:'style-loader',
-          use:['css-loader','sass-loader'],
-        })
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } }
+        ]
       },
       {
         test : /\.jsx?/,
@@ -36,12 +37,13 @@ var config = {
   },
   devServer: {
     port: 8000,
-    hotOnly: true
+    hotOnly: true,
+    inline: false
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
-    new ExtractTextPlugin({
+    new MiniCssExtractPlugin({
       filename:'index.css?r=[hash:12]'
     }),
     new HtmlWebpackPlugin({
